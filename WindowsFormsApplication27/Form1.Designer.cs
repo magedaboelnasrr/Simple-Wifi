2eb4d85ca295d92a1c3ac417f9f7f1f54310b1ef
magedaboelnasrr <maged.aboelnasr@gmail.com>
2021-08-13 16:21:37 +0200

    Simple Wifi

---
+++ WindowsFormsApplication27/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApplication27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "اتصال";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "الغاء الاتصال";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(459, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(574, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "كلمة المرور";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 46);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listView1.Size = new System.Drawing.Size(441, 239);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "اسم الشبكة";
+            this.columnHeader1.Width = 252;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "قوة الاشارة";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 183;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "..";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 333);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication27/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleWifi;
+
+namespace WindowsFormsApplication27
+{
+    
+    public partial class Form1 : Form
+    {
+        private Wifi wifi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool connectwifi(AccessPoint ap,string password)
+        {
+            AuthRequest authrequest = new AuthRequest(ap);
+            authrequest.Password = password;
+            return ap.Connect(authrequest);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wifi = new Wifi();
+            List<AccessPoint> aps = wifi.GetAccessPoints();
+            foreach (AccessPoint ap in aps)
+            {
+                ListViewItem listobj = new ListViewItem(ap.Name);
+                listobj.SubItems.Add(ap.SignalStrength + "%");
+                listobj.Tag = ap;
+                listView1.Items.Add(listobj);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count>0&& textBox1.Text.Length>0)
+            {
+                ListViewItem selecteditem = listView1.SelectedItems[0];
+                AccessPoint ap = (AccessPoint)selecteditem.Tag;
+                if (connectwifi(ap, textBox1.Text))
+                {
+                    label2.Text = ap.Name +"تم الاتصال بــ"  ; 
+                } 
+                else 
+                { label2.Text = ap.Name+ "تعذر الاتصال بــ";}
+              
+            }
+            else
+            { label2.Text = "اكتب كلمة المرور او اختر شبكة"; }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (wifi.ConnectionStatus == WifiStatus.Connected) { wifi.Disconnect(); label2.Text = "تم فطع الاتصال"; }
+        }
+
+       
+    }
+}
